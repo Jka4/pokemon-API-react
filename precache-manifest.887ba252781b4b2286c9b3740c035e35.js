51ca701091c37ace24d74bf0c7eee65234b4bbd7
jka4 <plotnik1295@gmail.com>
2020-01-13 11:42:47 +0200

    Updates

---
+++ precache-manifest.887ba252781b4b2286c9b3740c035e35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d148c3c05907df6047cb608f45d0cdc",
+    "url": "/pokemon-API-react/index.html"
+  },
+  {
+    "revision": "4f784a1427146a8dd7d5",
+    "url": "/pokemon-API-react/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "f5f2187cb1870911411e",
+    "url": "/pokemon-API-react/static/css/main.957e616d.chunk.css"
+  },
+  {
+    "revision": "4f784a1427146a8dd7d5",
+    "url": "/pokemon-API-react/static/js/2.d1c0899a.chunk.js"
+  },
+  {
+    "revision": "f5f2187cb1870911411e",
+    "url": "/pokemon-API-react/static/js/main.ce2317a9.chunk.js"
+  },
+  {
+    "revision": "59da5b1de4de189d467d",
+    "url": "/pokemon-API-react/static/js/runtime~main.13410119.js"
+  }
+]);
