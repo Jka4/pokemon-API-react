81457e20fb528d7a94d6efdf348d6746e23c6fed
jka4 <plotnik1295@gmail.com>
2020-07-22 11:41:10 +0300

    Updates

--- precache-manifest.c51f758cf5d7a6a45b7298053df66c8e.js
+++ precache-manifest.c51f758cf5d7a6a45b7298053df66c8e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "28241dbdf7d5ae1b8853c846b90d2d5a",
+    "revision": "5c434dd4f2b666f1f43ef9d1e2be42bb",
     "url": "/pokemon-API-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pokemon-API-react/static/css/2.f87e9f44.chunk.css"
   },
   {
-    "revision": "c8242e353972dae24413",
+    "revision": "2fe4de488076ba1908c0",
     "url": "/pokemon-API-react/static/css/main.7f5764dd.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pokemon-API-react/static/js/2.347beffc.chunk.js"
   },
   {
-    "revision": "c8242e353972dae24413",
-    "url": "/pokemon-API-react/static/js/main.82564d48.chunk.js"
+    "revision": "2fe4de488076ba1908c0",
+    "url": "/pokemon-API-react/static/js/main.563b4615.chunk.js"
   },
   {
     "revision": "59da5b1de4de189d467d",
