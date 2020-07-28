e4f6101e1595eb59a35655b22f5474a071448292
jka4 <plotnik1295@gmail.com>
2020-07-28 11:27:14 +0300

    wrap in docker

---
+++ build/precache-manifest.3f29d28c4fab52b5400adb7421581968.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2518172c47e94f90dd550da0dfa49b7b",
+    "url": "/pokemon-API-react/index.html"
+  },
+  {
+    "revision": "7b3f16d84b88d37b293a",
+    "url": "/pokemon-API-react/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "347b309eff75e794d609",
+    "url": "/pokemon-API-react/static/css/main.77073e42.chunk.css"
+  },
+  {
+    "revision": "7b3f16d84b88d37b293a",
+    "url": "/pokemon-API-react/static/js/2.f047c702.chunk.js"
+  },
+  {
+    "revision": "347b309eff75e794d609",
+    "url": "/pokemon-API-react/static/js/main.ffd86e2e.chunk.js"
+  },
+  {
+    "revision": "59da5b1de4de189d467d",
+    "url": "/pokemon-API-react/static/js/runtime~main.13410119.js"
+  }
+]);
