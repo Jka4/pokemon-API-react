de1e3b21c1d021439ebd2a698560d2842df913fb
jka4 <plotnik1295@gmail.com>
2020-07-31 14:02:19 +0300

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokemon-API-react/precache-manifest.d78b10b15736727b0ec75f639aa73b31.js"
+  "/pokemon-API-react/precache-manifest.41d3059fd4259899e9dc9a3b1be2299e.js"
 );
 
 self.addEventListener('message', (event) => {
