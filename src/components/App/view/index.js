909d0486dc93e375aa3f29704d605c08444e8299
jka4 <plotnik1295@gmail.com>
2020-08-10 18:27:01 +0300

    add catalog (hidden page)

--- src/components/App/view/index.js
+++ src/components/App/view/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-// import { hot } from 'react-hot-loader/root';
 
 import "../styles/App.scss";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
@@ -8,6 +7,7 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import MainContainer from "@MainContainer";
 import DetailedPage from "@DetailedPage";
 import PokemonsPageAll from '@PokemonsPageAll';
+import Catalog from '../../Catalog/Catalog';
 
 const App = () => {
   return (
@@ -17,6 +17,12 @@ const App = () => {
           <Route exact path='/' component={MainContainer} />
           <Route path='/detailedPage/pokemon/' component={DetailedPage} />
           <Route path='/allPokemons' component={PokemonsPageAll} />
+
+
+          <Route path="/catalog" component={Catalog} />
+
+
+
           <Route path="/" component={() => <div>page not found</div>} />
         </Switch>
       </div>
@@ -25,4 +31,3 @@ const App = () => {
 }
 
 export default App;
-// export default hot(App);
