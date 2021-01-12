cbb583243788138caabd19dd0122efb7c5526bbf
Jka4 <plotnik1295@gmail.com>
2021-01-12 23:31:03 +0200

    Updates

---
+++ static/js/15.70711cba.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppokedex=this.webpackJsonppokedex||[]).push([[15],{158:function(e,c,s){},184:function(e,c,s){"use strict";s.r(c);var a=s(2);s(0),s(158);c.default=function(){return Object(a.jsxs)("div",{className:"errorPage",children:[Object(a.jsx)("div",{className:"errCode",children:"404"}),Object(a.jsx)("div",{className:"image",children:Object(a.jsx)("img",{loading:"lazy",src:"".concat("/pokemon-API-react","/images/404.png"),alt:"404_pickachu"})}),Object(a.jsx)("div",{className:"description",children:"Page Not Found"})]})}}}]);
+//# sourceMappingURL=15.70711cba.chunk.js.map
