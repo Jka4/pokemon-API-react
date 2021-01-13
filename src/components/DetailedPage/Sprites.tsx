b836dce1434d2dbd2d7e1484b28ada2c081ffea2
jka4 <plotnik1295@gmail.com>
2021-01-13 18:44:35 +0200

    update

--- src/components/DetailedPage/Sprites.tsx
+++ src/components/DetailedPage/Sprites.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 
 const Sprites: React.FC<Props> = ({ sprites = {} }: Props) => {
-	console.log("🚀 ~ file: Sprites.tsx ~ line 13 ~ sprites", sprites)
+
 	const fallbackSprites = () => {
 		return (
 			<SkeletonTheme color="#6cff79" highlightColor="yellow">
@@ -18,33 +18,39 @@ const Sprites: React.FC<Props> = ({ sprites = {} }: Props) => {
 		);
 	};
 
+	const PaperSprites = () => {
+		return (
+			[1, 2, 3, 4].map((
+				n: number, // trash code just for generate placeholder
+			) => (
+				<Paper elevation={3} key={n} className="block">
+					{fallbackSprites()}
+				</Paper>
+			))
+		);
+	};
+
+	const SpritesImg = () => {
+		return (
+			Object.keys(sprites).map((spriteName: string) => (
+				<div key={spriteName}>
+					{sprites[spriteName] && (
+						<Paper elevation={3} className="block">
+							<ImageContainer
+								url={sprites[spriteName]}
+								cn={sprites[spriteName]}
+								fallback={fallbackSprites()}
+							/>
+						</Paper>
+					)}
+				</div>
+			))
+		);
+	};
+
 	return (<>
 		<div className="imagesLine">
-			{sprites ? (
-				Object.keys(sprites).map((spriteName: string) => (
-					<>
-						{sprites[spriteName] && (
-							<Paper elevation={3} className="block" key={spriteName}>
-								<ImageContainer
-									url={sprites[spriteName]}
-									cn={sprites[spriteName]}
-									fallback={fallbackSprites()}
-								/>
-							</Paper>
-						)}
-					</>
-				))
-			) : (
-					<>
-						{[1, 2, 3, 4].map((
-							n: number, // trash code just for generate placeholder
-						) => (
-							<Paper elevation={3} key={n} className="block">
-								{fallbackSprites()}
-							</Paper>
-						))}
-					</>
-				)}
+			{sprites ? SpritesImg() : PaperSprites()}
 		</div>
 	</>);
 };
