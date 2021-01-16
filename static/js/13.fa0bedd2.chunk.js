c9bd7b648c1d9f168e4aba2f506b421a7bc4bf19
jka4 <plotnik1295@gmail.com>
2021-01-16 16:21:37 +0200

    Updates

---
+++ static/js/13.fa0bedd2.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppokedex=this.webpackJsonppokedex||[]).push([[13],{142:function(e,c,s){},169:function(e,c,s){"use strict";s.r(c);var a=s(3);s(0),s(142);c.default=function(){return Object(a.jsxs)("div",{className:"errorPage",children:[Object(a.jsx)("div",{className:"errCode",children:"404"}),Object(a.jsx)("div",{className:"image",children:Object(a.jsx)("img",{loading:"lazy",src:"".concat("/pokemon-API-react","/images/404.png"),alt:"404_pickachu"})}),Object(a.jsx)("div",{className:"description",children:"Page Not Found"})]})}}}]);
+//# sourceMappingURL=13.fa0bedd2.chunk.js.map
