51e2b3420b4a47861f1e8f7fc377ae7704884662
jka4 <plotnik1295@gmail.com>
2021-02-10 18:27:17 +0200

    add version for chaching

--- src/Store/localStorage.ts
+++ src/Store/localStorage.ts
@@ -1,14 +1,17 @@
 import { PokesTypes } from 'types/index';
+import { version } from '../../package.json';
 
 type stateType = {
   randomPokemon: PokesTypes[];
   pokemonArr: PokesTypes[];
 };
 
+const keyLiteral = `pokemon_state_${version}`;
+
 export const saveState = (state: stateType) => {
   try {
     const serializedState = JSON.stringify(state);
-    localStorage.setItem('pokemon_state', serializedState);
+    localStorage.setItem(keyLiteral, serializedState);
   } catch (error) {
     console.log('Ooops, state not saved to localStorage');
     console.log('=> ', error);
@@ -17,7 +20,7 @@ export const saveState = (state: stateType) => {
 
 export const loadState = () => {
   try {
-    const serializedState = localStorage.getItem('pokemon_state');
+    const serializedState = localStorage.getItem(keyLiteral);
     if (serializedState === null) {
       return undefined;
     }
