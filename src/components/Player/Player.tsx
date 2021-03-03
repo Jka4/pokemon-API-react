53c7d62b0f5926ae4cb32ae35fc821aa5bd613af
jka4 <plotnik1295@gmail.com>
2021-03-03 18:08:45 +0200

    update

--- src/components/Player/Player.tsx
+++ src/components/Player/Player.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import useSound from 'use-sound';
 import { Button } from '@material-ui/core';
 import PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
@@ -6,21 +6,14 @@ import PauseCircleFilledIcon from '@material-ui/icons/PauseCircleFilled';
 
 const Player: React.FC = () => {
   const [play, { stop, isPlaying }] = useSound(`${process.env.PUBLIC_URL}/sound/pokemon_opening.mp3`);
-  const [isPlay, setIsPlay] = useState(false); // hook just for test cause useSound cant show current state
 
   const playPause = () => {
-    if (isPlaying) {
-      stop();
-      setIsPlay(false);
-    } else {
-      play();
-      setIsPlay(true);
-    }
+    isPlaying ? stop() : play();
   };
 
   return (
     <Button onClick={playPause} variant="contained" color="secondary">
-      {isPlay ? <PauseCircleFilledIcon /> : <PlayCircleFilledIcon />}
+      {isPlaying ? <PauseCircleFilledIcon /> : <PlayCircleFilledIcon />}
     </Button>
   );
 };
--- src/components/Player/__tests__/Player.test.tsx
+++ src/components/Player/__tests__/Player.test.tsx
@@ -1,36 +1,12 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
+
+import { mount } from 'enzyme';
 
 import Player from '../Player';
 
 describe('<Player />', () => {
-  let container: any;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
-  it('renders correctly', async () => {
-    await act(async () => {
-      ReactDOM.render(<Player />, container);
-      const button = await container.querySelector('button');
-      expect(button).toMatchSnapshot();
-    });
-  });
-
-  it('Player click', async () => {
-    await act(async () => {
-      ReactDOM.render(<Player />, container);
-      const button = await container.querySelector('button');
-      await button.dispatchEvent(new MouseEvent('click', { bubbles: true }));
-      expect(button).toMatchSnapshot();
-    });
+  it('renders correctly', () => {
+    const container = mount(<Player />);
+    expect(container).toMatchSnapshot();
   });
 });
